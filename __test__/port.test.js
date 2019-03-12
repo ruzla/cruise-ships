e153684d5eef5ad4a8cc1c9ccdfc0c5d14fa5c30
Russell <russelldoe@hotmail.co.uk>
2019-03-12 06:14:58 +0000

    edited readme and port test file

--- __test__/port.test.js
+++ __test__/port.test.js
@@ -11,19 +11,6 @@ describe('new port', () => {
   });
 });
 
-describe('Port Constructor', () => {
-  const port = new Port('Dublin');
-  const mockShip = jest.fn();
-  const mockShip2 = jest.fn();
-
-  it('removeShip method', () => {
-    port.ships = [mockShip2, mockShip];
-    port.removeShip(mockShip);
-    expect(port.ships).toEqual([mockShip2]);
-    expect(port.ships).not.toContain(mockShip);
-  });
-});
-
 describe('add ship to port', () => {
   it('returns port', () => {
     const port = new Port('Calais');
